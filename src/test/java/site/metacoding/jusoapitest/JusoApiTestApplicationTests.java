e90926071d337e36193755bcdfb84ca4950439c8
tjdgus7562 <tjdgus7562@naver.com>
2022-05-23 12:21:10 +0900

    주소 기초 세팅 완료

---
+++ src/test/java/site/metacoding/jusoapitest/JusoApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.jusoapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JusoApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
